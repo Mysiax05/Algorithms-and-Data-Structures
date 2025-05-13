45057865442d7b9ab762c93fe109d24ecfcb21a9
Hubert Myszka <hubert.myszka0@gmail.com>
2025-05-13 21:19:28 +0200

    Topological Sorting Algorithm for Directed Acyclic Graph (DAG)

--- Graphs/Algorithms/Topological_sort.py
+++ Graphs/Algorithms/Topological_sort.py
@@ -1,7 +1,7 @@
 # Directed Acyclic Graph
 
 # adjacency list O(V+E)
-def top_sort_list(G,s):
+def top_sort_list(G):
     def dfs_visit(G,u):
         visited[u] = True
         for v in G[u]:
@@ -16,5 +16,25 @@ def top_sort_list(G,s):
     for u in range(V):
         if not visited[u]:
             dfs_visit(G,u)
+    
+    return result[::-1]
+
+# adjacency matrix O(V^2)
+def top_sort_matrix(G):
+    def dfs_vist(G,u):
+        visited[u] = True
+        for v in range(len(G)):
+            if G[u][v] == 1 and not visited[v]:
+                dfs_vist(G,v)
+        result.append(u)
+    # end def
+
+    V = len(G)
+    visited = [False] * V
+    result = []
+
+    for u in range(V):
+        if not visited[u]:
+            dfs_vist(G,u)
     
     return result[::-1]
