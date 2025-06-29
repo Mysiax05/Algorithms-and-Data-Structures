71dacbc867554ebb4925d21a9ef744a8a8ce3378
Hubert Myszka <hubert.myszka0@gmail.com>
2025-06-29 13:32:04 +0200

    Dijkstra Algorithm

--- Graphs/Algorithms/Dijkstra.py
+++ Graphs/Algorithms/Dijkstra.py
@@ -23,23 +23,25 @@ def dijkstra_list(G,s):
 
 # adjacency matrix O(V^2)
 def dijkstra_matrix(G,s):
-    from queue import PriorityQueue
     V = len(G)
-    q = PriorityQueue()
     visited = [False] * V
     parent = [None] * V
     dist = [float('inf')] * V
-
+    
     dist[s] = 0
-    q.put((0,s))
-    while not q.empty():
-        d,u = q.get()
-        if visited[u]:
-            continue
+    for _ in range(V):
+        u = -1
+        min_dist = float('inf')
+        for v in range(V):
+            if not visited[v] and dist[v] < min_dist:
+                u = v
+                min_dist = dist[v]
+        
+        if u == -1:
+            break
+
         visited[u] = True
         for v in range(V):
             if G[u][v] != float('inf') and dist[v] > dist[u] + G[u][v]:
-                parent[v] = u
                 dist[v] = dist[u] + G[u][v]
-                q.put((dist[v],v))
-    return dist, parent
+                parent[v] = u
